deb955715e569042309994a052bf0fb75519a50d
sarveshsta <sarvesh@seizetheads.com>
2024-02-15 18:28:50 +0530

    is_superuser fixed

--- algo_app/models.py
+++ algo_app/models.py
@@ -23,6 +23,7 @@ class User(AbstractBaseUser, PermissionsMixin, Timestamps):
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     
     USERNAME_FIELD = 'phone'
     REQUIRED_FIELDS = ['phone', 'name']
