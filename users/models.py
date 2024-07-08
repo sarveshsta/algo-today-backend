79a42788f4a50b2a6f82c5d1cf1470dd5912940f
sarveshdubey <sarvesh91199@gmail.com>
2024-07-08 17:36:53 +0530

    changed related name in user strategy

--- users/models.py
+++ users/models.py
@@ -56,7 +56,7 @@ class Strategy(Timestamps):
     strategy_id = models.CharField(max_length=40, default=None, blank=True, null=True)
     description = models.TextField()
     user = models.ForeignKey(User, verbose_name='User-Strategy', blank=True, 
-                            on_delete=models.CASCADE, related_name='user_strategy')
+                            on_delete=models.CASCADE, related_name='algotrade_user_strategy')
     advantages = models.TextField()
     
 
